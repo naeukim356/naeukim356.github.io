fa8535543f5c8cde16f9e33a4151d429e3ef67bb
naeukim356 <83847763+naeukim356@users.noreply.github.com>
2021-05-15 12:02:17 +0900

    web designy thingie

---
+++ index.js
@@ -0,0 +1,52 @@
+window.onload = function () {
+    var desc = ["설명", "Hi", "hehe"];
+var index = 0;
+
+var interval = setInterval(function() {
+    $('#book1_desc').fadeOut(500, function() {
+        index++;
+        if (index === desc.length)
+        index = 0;
+        $(this).text(desc[index]).fadeIn(500);
+    });
+}, 4000);
+
+var leftarrow = document.getElementById('left-arrow');
+var rightarrow = document.getElementById('right-arrow');
+
+leftarrow.onclick = function () {
+    $('#book1_desc').fadeOut(500,function() {
+        index--;
+        if (index === -1)
+            index = 2;
+        $(this).text(desc[index]).fadeIn(500);
+        clearInterval(interval);
+        interval = setInterval(function() {
+            $('#book1_desc').fadeOut(500, function() {
+                index++;
+                if (index === desc.length)
+                index = 0;
+                $(this).text(desc[index]).fadeIn(500);
+            });
+        }, 4000);
+    })
+}
+rightarrow.onclick = function () {
+    $('#book1_desc').fadeOut(500,function() {
+        index++;
+        if (index === desc.length)
+            index = 0;
+        $(this).text(desc[index]).fadeIn(500);
+        clearInterval(interval);
+        interval = setInterval(function() {
+            $('#book1_desc').fadeOut(500, function() {
+                index++;
+                if (index === desc.length)
+                index = 0;
+                $(this).text(desc[index]).fadeIn(500);
+            });
+        }, 4000);
+    })
+}
+}
+
